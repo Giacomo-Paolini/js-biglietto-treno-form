a5cd9ce2619ba453ce37e126fa1337db65653dbc
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-04-17 15:13:36 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 document.body.style.backgroundColor = "#B91450";
 
 function priceTicket() {
+    let name = document.getElementById("name").value;
     let km = parseInt(document.getElementById("km").value);
     let age = document.getElementById("age").value;
     let price = km * 0.21;
-    let name = document.getElementById("name").value;
     if (age == "1") {
-        price -= (price / 100 * 20);
+        price -= price / 100 * 20;
     }   else if (age == "3") {
-        price -= (price / 100 * 40);
+        price -= price / 100 * 40;
     }
     price = price.toFixed(2);
     document.getElementById("price").innerText = price + " €";
